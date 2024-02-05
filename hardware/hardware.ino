86d54ee62d8378551c0cdb01ed75aa4a34bb0736
Giovanni Lawrence <isekcftw@gmail.com>
2024-02-05 02:22:54 -0400

    Hardware is aiite ig

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -9,7 +9,8 @@
 #include <ctype.h>
 
 // ADD YOUR IMPORTS HERE
-
+#include <FastLED.h>
+#include <DHT.h>
 
 
 #ifndef _WIFI_H 
@@ -37,20 +38,25 @@
 // DEFINE VARIABLES
 #define ARDUINOJSON_USE_DOUBLE      1 
 
-// DEFINE THE CONTROL PINS FOR THE DHT22 
+// DEFINE THE CONTROL PINS FOR THE DHT22
+#define DHTPIN 32
+#define DHTTYPE DHT22
+#define DATA_PIN 21
+#define NUM_LEDS 7
+
 
 
 
 
 // MQTT CLIENT CONFIG  
-static const char* pubtopic      = "620012345";                    // Add your ID number here
-static const char* subtopic[]    = {"620012345_sub","/elet2415"};  // Array of Topics(Strings) to subscribe to
+static const char* pubtopic      = "620146473";                    // Add your ID number here
+static const char* subtopic[]    = {"620146473_sub","/elet2415"};  // Array of Topics(Strings) to subscribe to
 static const char* mqtt_server   = "local";         // Broker IP address or Domain name as a String 
 static uint16_t mqtt_port        = 1883;
 
 // WIFI CREDENTIALS
-const char* ssid       = "YOUR_SSID";     // Add your Wi-Fi ssid
-const char* password   = "YOUR_PASSWORD"; // Add your Wi-Fi password 
+const char* ssid       = "MonaConnect";     // Add your Wi-Fi ssid
+const char* password   = ""; // Add your Wi-Fi password 
 
 
 
@@ -81,7 +87,8 @@ double calcHeatIndex(double Temp, double Humid);
 
 
 /* Init class Instances for the DHT22 etcc */
- 
+DHT dht(DHTPIN, DHTTYPE);
+CRGB leds[NUM_LEDS];
   
 
 //############### IMPORT HEADER FILES ##################
@@ -102,7 +109,8 @@ void setup() {
   // INITIALIZE ALL SENSORS AND DEVICES
   
   /* Add all other necessary sensor Initializations and Configurations here */
-
+  LEDS.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  dht.begin();
 
   initialize();     // INIT WIFI, MQTT & NTP 
   // vButtonCheckFunction(); // UNCOMMENT IF USING BUTTONS INT THIS LAB, THEN ADD LOGIC FOR INTERFACING WITH BUTTONS IN THE vButtonCheck FUNCTION
@@ -128,7 +136,7 @@ void vButtonCheck( void * pvParameters )  {
     for( ;; ) {
         // Add code here to check if a button(S) is pressed
         // then execute appropriate function if a button is pressed  
-
+        
         vTaskDelay(200 / portTICK_PERIOD_MS);  
     }
 }
@@ -143,31 +151,41 @@ void vUpdate( void * pvParameters )  {
    
           // 1. Read Humidity and save in variable below
           double h = 0;
+          h = dht.readHumidity();
            
           // 2. Read temperature as Celsius   and save in variable below
-          double t = 0;    
- 
+          double t = 0;
+          t = dht.readTemperature();  
+
 
           if(isNumber(t)){
               // ##Publish update according to ‘{"id": "student_id", "timestamp": 1702212234, "temperature": 30, "humidity":90, "heatindex": 30}’
 
               // 1. Create JSon object
+              StaticJsonDocument<1000> doc;
               
               // 2. Create message buffer/array to store serialized JSON object
+              char message[1100]  = {0};
               
               // 3. Add key:value pairs to JSon object based on above schema
+              doc["id"]           = "620146473";
+              doc["timestamp"]    = getTimeStamp();
+              doc["temperature"]  = t;
+              doc["humidity"]     = h;
+              doc["heatindex"]    = calcHeatIndex(t, h);
 
               // 4. Seralize / Covert JSon object to JSon string and store in message array
+              serializeJson(doc, message);
                
-              // 5. Publish message to a topic sobscribed to by both backend and frontend                
-
+              // 5. Publish message to a topic sobscribed to by both backend and frontend
+              if (mqtt.connected()) {
+                publish(pubtopic, message);
+              }
           }
-
-          
-            
-        vTaskDelay(1000 / portTICK_PERIOD_MS);  
-    }
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }               
 }
+
 
  
 
@@ -206,15 +224,31 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 
   // PROCESS MESSAGE
-  const char* type = doc["type"]; 
-
+  const char* type = doc["type"];
+  
   if (strcmp(type, "controls") == 0){
     // 1. EXTRACT ALL PARAMETERS: NODES, RED,GREEN, BLUE, AND BRIGHTNESS FROM JSON OBJECT
+    int nodes = doc["leds"];
+    int brightness = doc["brightness"];
+    int red = doc["color"]["r"];
+    int green = doc["color"]["g"];
+    int blue = doc["color"]["b"];
 
     // 2. ITERATIVELY, TURN ON LED(s) BASED ON THE VALUE OF NODES. Ex IF NODES = 2, TURN ON 2 LED(s)
+    for (unsigned char i = 0; i < nodes; i++) {
+      leds[i] = CRGB(red, green, blue);
+      FastLED.setBrightness(brightness);
+      FastLED.show();
+      delay(50);
+    }
 
     // 3. ITERATIVELY, TURN OFF ALL REMAINING LED(s).
-   
+    for (unsigned char x = nodes; x < NUM_LEDS; x++) {
+      leds[x] = CRGB::Black;
+      FastLED.setBrightness(brightness);
+      FastLED.show();
+      delay(50);
+    }
   }
 }
 
@@ -239,15 +273,31 @@ bool publish(const char *topic, const char *payload){
 //***** Complete the util functions below ******
 
 double convert_Celsius_to_fahrenheit(double c){    
-    // CONVERTS INPUT FROM °C TO °F. RETURN RESULTS     
+    // CONVERTS INPUT FROM °C TO °F. RETURN RESULTS
+    return (c * 9/5) + 32;
+
 }
 
 double convert_fahrenheit_to_Celsius(double f){    
-    // CONVERTS INPUT FROM °F TO °C. RETURN RESULT    
+    // CONVERTS INPUT FROM °F TO °C. RETURN RESULT
+    return (5 / 9) * (f - 32);
 }
 
 double calcHeatIndex(double Temp, double Humid){
     // CALCULATE AND RETURN HEAT INDEX USING EQUATION FOUND AT https://byjus.com/heat-index-formula/#:~:text=The%20heat%20index%20formula%20is,an%20implied%20humidity%20of%2020%25
+    
+    const double c1 = -42.379; const double c2 = 2.04901523; const double c3 = 10.14333127;
+    const double c4 = -0.22475541; const double c5 = -6.83783e-03; const double c6 = -5.481717e-02;
+    const double c7 = 1.22874e-03; const double c8 = 8.5282e-04; const double c9 = -1.99e-06;
+
+    double T = Temp * 9 / 5 + 32;
+
+    double HI = c1 + (c2 * T) + (c3 * Humid) + (c4 * T * Humid) + 
+    (c5 * T * T) + (c6 * Humid * Humid) + (c7 * T * T * Humid) + 
+    (c8 * T * Humid * Humid) + (c9 * T * T * Humid * Humid);
+
+    return HI;
+
   
 }
  
